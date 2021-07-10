2ea3a6216a261f0c89ab3914250683356b435a22
Susanne85 <xkqk2010@gmail.com>
2021-07-10 21:41:58 +0800

    Create workout

--- routes/api/api.js
+++ routes/api/api.js
@@ -14,11 +14,27 @@ router.get("/workouts", (request, response) => {
 })
 
 router.put("/workouts/:id", (request, response) => {
+    console.log("Route api/workouts/:id - add new exercise", request.params.id);
+});
+
+router.put("/workouts/", (request, response) => {
     console.log("Route api/workouts/:id - add new exercise");
 })
 router.post("/workouts", (request, response) => {
-    console.log("Route api/workouts - add new workout");
-})
+    console.log("Route api/workouts - add new exercise");
+    const exercise = request.body;
+    console.log("Exercise data, adding a new record", exercise);
+    const addOne = Workout.insertOne(request.body, (error, data) => {
+        if (error) {
+            response.status(500);
+            response.send(error.message);
+        } else {
+            console.log('object type ', typeof data)
+            response.send(data);
+        }
+    });
+});
+
 
 router.get("/workouts/range", (request, response) => {
     console.log("Route api/workouts - workouts in range");
