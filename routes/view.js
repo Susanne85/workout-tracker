a041d526f9641ad3f8624bc6f3e93fc35256d335
Susanne85 <xkqk2010@gmail.com>
2021-07-12 13:50:34 +0800

    Commented out console log statements

--- routes/view.js
+++ routes/view.js
@@ -2,17 +2,17 @@ const router = require('express').Router();
 const path = require('path');
 
 router.get('/', (request, response) => {
-    console.log('Route View -  home');
+    //console.log('Route View -  home');
     response.sendFile(path.join(__dirname + '/../public/index.html'));
 })
 
 router.get('/exercise', (request, response) => {
-    console.log('Route View -  exercise');
+    //console.log('Route View -  exercise');
     response.sendFile(path.join(__dirname + '/../public/exercise.html'));
 })
 
 router.get('/stats', (request, response) => {
-    console.log('Route View - stats');
+    //console.log('Route View - stats');
     response.sendFile(path.join(__dirname + '/../public/stats.html'));
 })
 module.exports = router
